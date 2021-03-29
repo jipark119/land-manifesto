5fcf02b0892366976d315d5904f33dc824f4d850
jipark119 <jp5677@nyu.edu>
2021-03-29 15:33:41 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,47 @@
+function oneOpen() {
+    var x = document.getElementById("one");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function twoOpen() {
+    var x = document.getElementById("two");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function threeOpen() {
+    var x = document.getElementById("three");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function fourOpen() {
+    var x = document.getElementById("four");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function fiveOpen() {
+    var x = document.getElementById("five");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
